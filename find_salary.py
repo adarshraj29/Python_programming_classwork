b8b704cd92dd49308506d90c4459302e4b2c96c4
Adarsh raj <rajadarsh232@gmail.com>
2024-05-24 07:36:03 +0000

    find net salery and other allowances

---
+++ find_salary.py
@@ -0,0 +1,11 @@
+a=input ("enter your name")
+b= int(input("enter your id"))
+c= input("enter your post")
+d=int(input("eneter your basic salary"))
+hra=d*30/100
+da=d*20/100
+ma=d*15/100
+ta=d*25/100
+pf=d*40/100
+gs=hra+da+ta+ma-pf
+print("name=",a,"\n empolyee id=",b,"\n post=",c,"\n net salary=",d,"\n hra=",hra,"\n da=",da,"\n ma=",ma,"\n ta=",ta,)
